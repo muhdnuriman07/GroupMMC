2c1a2139519b9df42234034ca82f800f7731e115
MuhdNuriman <64823658+muhdnuriman07@users.noreply.github.com>
2020-07-29 14:37:13 +0800

    Update Lab9B and 9C.py

--- Lab9B and 9C.py
+++ Lab9B and 9C.py
@@ -1,7 +1,6 @@
-
 #Lab 9B and 9C
-try:
- #Where AF_INET specify IPv4
+ try:
+#Where AF_INET specify IPv4
  s = socket.socket(socket.AF_INET,socket.SOCK_STREAM);
  print("Socket created succefully")
  
@@ -16,7 +15,16 @@ try:
   print("There is a problem resolving the host")
   sys.exit();
   
- #connect to the web Server
+#connect to the web Server
  s.connect((host_ip,port));
  print("Successfully connected to the website given %s:%s"%(host_ip,port));
+ 
+#End of function
+parser = argparse.ArgumentParser(description="Learn to connect to a website with port number");
+parser.add_argument("-H", "--host", required=True, help="specify the hostname or website name");
+parser.add_argument("-p", "--port", type=int, default=80, help="specify the port number eg. 80 for website");
+
+args = parser.parse_args();
+ploadconnect(args.host,args.port);
+
                               
