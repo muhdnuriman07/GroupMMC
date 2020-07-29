828ebe15cdf71421e6616d738827307cdd0ca526
Damienngo <64823488+Damienngo@users.noreply.github.com>
2020-07-29 14:19:08 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,5 +1,6 @@
 # First import the socket library
 import socket;
+import sys
 
 #Create a socket object
 s = socket.socket();
@@ -23,3 +24,24 @@ while True:
  c, addr = s.accept();
  print("Connection from",addr);
 
+#Lab 9B
+try:
+ #Where AF_INET specify IPv4
+ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+ print("Socket created succefully")
+ 
+ except socket.error as err:
+  print("Socket creation failed with error %s"%(err)):
+   
+#Default web port
+port=80;
+try:
+ host_ip = socket.gethostbyname("www.google.com")
+ except socket.gaierror:
+  print("There is a problem resolving the host")
+  sys.exit();
+  
+ #connect to the web Server
+ s.connect((host_ip,port));
+ print("Successfully connected to the website given %s:%s"%(host_ip,port));
+                              
